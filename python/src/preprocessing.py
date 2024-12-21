fixed padding, added elasticity calculation

python/src/preprocessing.py
@@ -19,7 +19,7 @@ def compile_quality(species_name, D_m, resolution):
     # loading fine resolution raster
     raster_NSDM = NSDM().load_raster(species_name)
     lat_resolution, lon_resolution = calculate_resolution(raster_NSDM)
-    print(f"Resolution: {lon_resolution/1000:0.3f}km")
+    print(f"Original quality resolution: {lon_resolution/1000:0.3f}km")
     assert lat_resolution == lon_resolution
     resampling_factor = int(np.ceil(resolution/lat_resolution))
     raster_NSDM = coarsen_raster(raster_NSDM, resampling_factor)
@@ -67,13 +67,45 @@ def compile_resistance(quality, barriers, eps =  1e-5):
     return final_resistance.rio.set_crs(quality.rio.crs)
 
 
-def padding(quality, buffer_size, batch_size):
-    pad_height = (quality.shape[0] + 2 * buffer_size) % batch_size
-    pad_width = (quality.shape[1] + 2 * buffer_size) % batch_size
+def padding(raster, buffer_size, window_size):
+    """
+    Pads the given raster array to ensure its dimensions are compatible with the
+    specified window size, i.e. assert (raster.shape[i] - 2 * buffer_size) %
+    window_size == 0
+    """
+    inner_height = raster.shape[0] - 2 * buffer_size
+    inner_width = raster.shape[1] - 2 * buffer_size
 
-    padded_quality = jnp.pad(
-        quality,
-        (pad_height, pad_width),
+    pad_height = (window_size - (inner_height % window_size)) % window_size
+    pad_width = (window_size - (inner_width % window_size)) % window_size
+
+
+    padded_raster = jnp.pad(
+        raster,
+        ((0,pad_height),(0,pad_width)),
         mode='constant'
     )
-    return padded_quality
+    return padded_raster
+
+if __name__ == "__main__":
+    from jaxscape.moving_window import WindowOperation
+
+    def test_padding():
+        raster = jnp.ones((2300, 3600))
+        buffer_size = 50
+        window_size = 3
+        padded_raster = padding(raster, buffer_size, window_size)
+        
+        for i in range(2):
+            assert (padded_raster.shape[i] - 2 * buffer_size) % window_size == 0
+        
+        # other test
+        raster = jnp.ones((230, 360))
+        buffer_size = 3
+        window_size = 27
+        padded_raster = padding(raster, buffer_size, window_size)
+
+        for i in range(2):
+            assert (padded_raster.shape[i] - 2 * buffer_size) % window_size == 0
+        
+    test_padding()