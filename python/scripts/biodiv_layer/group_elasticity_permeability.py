fixed bug calculation

python/scripts/biodiv_layer/group_elasticity_permeability.py
@@ -1,5 +1,6 @@
 """
-Calculating the elasticity of habitat quality with respect to permeability using Jaxscape.
+Calculating the elasticity of habitat quality with respect to permeability for
+all taxonomic group considered using `jaxscape`.
 """
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '1'  # Use the first GPU
@@ -20,7 +21,7 @@ import equinox as eqx
 from tqdm import tqdm
 import sys
 sys.path.append(str(Path(__file__).parent / Path("../../src/")))
-from preprocessing import compile_group_suitability
+from preprocessing import compile_group_suitability, CRS_CH
 from processing import batch_run_calculation, padding, GROUP_INFO
 from postprocessing import postprocess
 import xarray as xr
@@ -37,6 +38,15 @@ def qKqT(permeability, hab_qual, activities, distance, D):
 
     window_center = jnp.array([[activities.shape[0]//2+1, activities.shape[1]//2+1]])
     q = grid.array_to_node_values(hab_qual)
+    
+    # NOTE: the distance should be calculated from all the pixels to the
+    # `window_center` yet the Bellman-Ford algorithm only works for the distance
+    # between a single source to all pixels. However, if the graph is symmetric,
+    # then the distance from all pixels to the window_center is the same as the
+    # distance from the window_center to all pixels we should make sure that the
+    # graph is symmetric, which is currently not the case as the adjacency
+    # matrix has `fun` defaulting to target pixel
+    # TODO: modify graph so that it is symetric
     dist = distance(grid, sources=window_center).reshape(-1)
 
     K = jnp.exp(-dist/D) # calculating proximity matrix
@@ -63,77 +73,79 @@ if __name__ == "__main__":
             }
 
     # # TODO: test to remove
-    # GROUP_INFO = {"Reptiles": LCPDistance()}
+    GROUP_INFO = {"Vascular_plants": EuclideanDistance()}
     for group in GROUP_INFO:
         print("Computing elasticity for group:", group)
         distance = GROUP_INFO[group]
-
-        output_path = Path("output") / group
-        output_path.mkdir(parents=True, exist_ok=True)
         
-        suitability_dataset = compile_group_suitability(group, 
-                                                        config["resolution"])
-        D_m = suitability_dataset.attrs["D_m"]
-        quality = jnp.array(suitability_dataset["mean_suitability"].values, dtype=config["dtype"])
-        quality = jnp.nan_to_num(quality, nan=0.0)
-        quality = jnp.where(quality == 0, 1e-5, quality)
-        assert jnp.all(quality > 0) and jnp.all(quality < 1) and jnp.all(jnp.isfinite(quality)), "Quality values must be between 0 and 1."
-        
-        ## Calculating meta parameters
-        # disepersal in pixels
-        D = np.array(D_m / config["resolution"], dtype=config["dtype"])
-        assert D >= 1, "Dispersal range must be greater than 1 pixel."
-        
-        # number of pixels -1 to skip per iteration and that will not be considered as landmarks
-        coarsening = int(jnp.ceil(D * config["coarsening_factor"]))
-        if coarsening % 2 == 0:
-            coarsening += 1
+        if not type(distance) is EuclideanDistance:
+            output_path = Path("output") / group
+            output_path.mkdir(parents=True, exist_ok=True)
             
-        # buffer size should be of the order of the dispersal range - half that of the window operation size
-        # size distance is calculated from the center pixel of the window
-        buffer_size = int(D - (coarsening - 1)/2)
-        if buffer_size < 1:
-            raise ValueError("Buffer size is too small. Consider decreasing the coarsening factor or decreasing the raster resolution.")
-        
-        batch_window_size = config["batch_size"] * coarsening
+            suitability_dataset = compile_group_suitability(group, 
+                                                            config["resolution"])
+            D_m = suitability_dataset.attrs["D_m"]
+            quality = jnp.array(suitability_dataset["mean_suitability"].values, dtype=config["dtype"])
+            quality = jnp.nan_to_num(quality, nan=0.0)
+            quality = jnp.where(quality == 0, 1e-5, quality)
+            assert jnp.all(quality > 0) and jnp.all(quality < 1) and jnp.all(jnp.isfinite(quality)), "Quality values must be between 0 and 1."
+            
+            ## Calculating meta parameters
+            # disepersal in pixels
+            D = np.array(D_m / config["resolution"], dtype=config["dtype"])
+            assert D >= 1, "Dispersal range must be greater than 1 pixel."
+            
+            # number of pixels -1 to skip per iteration and that will not be considered as landmarks
+            coarsening = int(jnp.ceil(D * config["coarsening_factor"]))
+            if coarsening % 2 == 0:
+                coarsening += 1
+                
+            # buffer size should be of the order of the dispersal range - half that of the window operation size
+            # size distance is calculated from the center pixel of the window
+            buffer_size = int(D - (coarsening - 1)/2)
+            if buffer_size < 1:
+                raise ValueError("Buffer size is too small. Consider decreasing the coarsening factor or decreasing the raster resolution.")
+            
+            batch_window_size = config["batch_size"] * coarsening
 
-        quality_padded = padding(quality, buffer_size, batch_window_size)
-        
-        batch_op = WindowOperation(
-            shape=quality_padded.shape, 
-            window_size=batch_window_size, 
-            buffer_size=buffer_size)
-        
-        output = jnp.zeros_like(quality_padded) # initialize raster
-        window_op = WindowOperation(shape=(batch_op.total_window_size, batch_op.total_window_size), 
-                                    window_size=coarsening, 
-                                    buffer_size=buffer_size)
-        for (xy_batch, permeability_batch) in tqdm(batch_op.lazy_iterator(quality_padded), desc="Batch progress", total=batch_op.nb_steps):
-            if not jnp.all(jnp.isnan(permeability_batch)):
-                xy, hab_qual = window_op.eager_iterator(permeability_batch)
-                activities = jnp.ones_like(hab_qual, dtype="bool")
-                permeability = hab_qual
-                res = batch_run_calculation(batch_op, window_op, xy, qKqT_grad_vmap, permeability, hab_qual, activities, distance, D)
-                output = batch_op.update_raster_with_window(xy_batch, output, res, fun=jnp.add)
-        
-        # unpadding
-        output = output[:quality.shape[0], :quality.shape[1]]
-        
-        elasticity = output * quality # quality == permeability
+            quality_padded = padding(quality, buffer_size, batch_window_size)
+            
+            batch_op = WindowOperation(
+                shape=quality_padded.shape, 
+                window_size=batch_window_size, 
+                buffer_size=buffer_size)
+            
+            output = jnp.zeros_like(quality_padded) # initialize raster
+            window_op = WindowOperation(shape=(batch_op.total_window_size, batch_op.total_window_size), 
+                                        window_size=coarsening, 
+                                        buffer_size=buffer_size)
+            for (xy_batch, permeability_batch) in tqdm(batch_op.lazy_iterator(quality_padded), desc="Batch progress", total=batch_op.nb_steps):
+                if not jnp.all(jnp.isnan(permeability_batch)):
+                    xy, hab_qual = window_op.eager_iterator(permeability_batch)
+                    activities = jnp.ones_like(hab_qual, dtype="bool")
+                    permeability = hab_qual
+                    res = batch_run_calculation(batch_op, window_op, xy, qKqT_grad_vmap, permeability, hab_qual, activities, distance, D)
+                    output = batch_op.update_raster_with_window(xy_batch, output, res, fun=jnp.add)
+            
+            # unpadding
+            output = output[:quality.shape[0], :quality.shape[1]]
+            
+            elasticity = output * quality # quality == permeability
 
-        # TODO: to remove
-        # fig, axes = plt.subplots(1, 2, figsize=(10, 5))
-        # im1 = axes[0].imshow(quality)
-        # axes[0].set_title("Quality")
-        # fig.colorbar(im1, ax=axes[0], shrink=0.1)
-        # im2 = axes[1].imshow(elasticity)
-        # axes[1].set_title("Elasticity w.r.t permeability")
-        # fig.colorbar(im2, ax=axes[1], shrink=0.1)
-        # plt.show()
-        
-        # TODO: tif and nc files not consistent
-        output_raster = deepcopy(suitability_dataset["mean_suitability"])
-        output_raster.values = elasticity
-        output_raster = postprocess(output_raster)
-        output_raster.rio.to_raster(output_path / "elasticity_permeability.tif", compress='lzw')
-        print("Saved elasticity raster at:", output_path / "elasticity_permeability.tif")
+            # TODO: to remove
+            # fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+            # im1 = axes[0].imshow(quality)
+            # axes[0].set_title("Quality")
+            # fig.colorbar(im1, ax=axes[0], shrink=0.1)
+            # im2 = axes[1].imshow(elasticity)
+            # axes[1].set_title("Elasticity w.r.t permeability")
+            # fig.colorbar(im2, ax=axes[1], shrink=0.1)
+            # plt.show()
+            
+            # TODO: tif and nc files not consistent
+            output_raster = deepcopy(suitability_dataset["mean_suitability"])
+            output_raster.rio.set_crs(CRS_CH, inplace=True)
+            output_raster.values = elasticity
+            output_raster = postprocess(output_raster)
+            output_raster.rio.to_raster(output_path / "elasticity_permeability.tif", compress='lzw')
+            print("Saved elasticity raster at:", output_path / "elasticity_permeability.tif")

python/scripts/biodiv_layer/group_elasticity_quality.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
         assert jnp.all(quality > 0) and jnp.all(quality < 1) and jnp.all(jnp.isfinite(quality)), "Quality values must be between 0 and 1."
         
         ## Calculating meta parameters
-        # disepersal in pixels
+        # dispersal in pixels
         D = np.array(D_m / config["resolution"], dtype=config["dtype"])
         assert D >= 1, "Dispersal range must be greater than 1 pixel."
         

python/src/preprocessing.py
@@ -13,8 +13,6 @@ from utils_raster import crop_raster, calculate_resolution, coarsen_raster, mask
 from TraitsCH import TraitsCH
 from NSDM import NSDM
 from EUSDM import EUSDM
-import jax.numpy as jnp
-import pickle 
 
 def compile_species_suitability(species_name, D_m, resolution):
     # loading fine resolution raster
@@ -82,56 +80,59 @@ def compile_group_suitability(group, resolution):
             continue
             # print(f"Failed to load raster for {sp}")
     print(f"Loaded {len(nsdm_rasters)} rasters")
+    if len(nsdm_rasters) == len(eu_sdm_rasters) > 0:
+        # Aggregating rasters
+        nsdm_stack = xr.concat(nsdm_rasters, dim="species")
+        mean_ch_sdm_suitability = nsdm_stack.mean(dim="species").squeeze("band").rename("mean_nsdm")    
+        std_ch_sdm_suitability = nsdm_stack.std(dim="species").squeeze("band").rename("std_nsdm")    
 
-    # Aggregating rasters
-    nsdm_stack = xr.concat(nsdm_rasters, dim="species")
-    mean_ch_sdm_suitability = nsdm_stack.mean(dim="species").squeeze("band").rename("mean_nsdm")    
-    std_ch_sdm_suitability = nsdm_stack.std(dim="species").squeeze("band").rename("std_nsdm")    
+        eu_sdm_stack = xr.concat(eu_sdm_rasters, dim="species")
+        mean_eu_sdm_suitability = eu_sdm_stack.mean(dim="species").rename("mean_eu_sdm") 
+        std_eu_sdm_suitability = eu_sdm_stack.std(dim="species").rename("std_eu_sdm")    
+        
+        # merging coarse and fine rasters
+        raster_coarse_interp_mean = mean_eu_sdm_suitability.interp_like(mean_ch_sdm_suitability, method="nearest")
+        combined_raster_mean = xr.where(mean_ch_sdm_suitability.notnull(), mean_ch_sdm_suitability, raster_coarse_interp_mean)
+        combined_raster_mean = combined_raster_mean.rename(species.iloc[0]) # TODO: maybe fix, needed for masking
+        combined_raster_mean.rio.set_crs(CRS_CH, inplace=True)
 
-    eu_sdm_stack = xr.concat(eu_sdm_rasters, dim="species")
-    mean_eu_sdm_suitability = eu_sdm_stack.mean(dim="species").rename("mean_eu_sdm") 
-    std_eu_sdm_suitability = eu_sdm_stack.std(dim="species").rename("std_eu_sdm")    
-    
-    # merging coarse and fine rasters
-    raster_coarse_interp_mean = mean_eu_sdm_suitability.interp_like(mean_ch_sdm_suitability, method="nearest")
-    combined_raster_mean = xr.where(mean_ch_sdm_suitability.notnull(), mean_ch_sdm_suitability, raster_coarse_interp_mean)
-    combined_raster_mean = combined_raster_mean.rename(species.iloc[0]) # TODO: maybe fix, needed for masking
-    combined_raster_mean.rio.set_crs(nsdm_rasters[0].rio.crs, inplace=True)
+        raster_coarse_interp_std = std_eu_sdm_suitability.interp_like(std_ch_sdm_suitability, method="nearest")
+        combined_raster_std = xr.where(std_ch_sdm_suitability.notnull(), std_ch_sdm_suitability, raster_coarse_interp_std)
+        combined_raster_std = combined_raster_std.rename(species.iloc[0]) # TODO: maybe fix, needed for masking
+        combined_raster_std.rio.set_crs(CRS_CH, inplace=True)
 
-    raster_coarse_interp_std = std_eu_sdm_suitability.interp_like(std_ch_sdm_suitability, method="nearest")
-    combined_raster_std = xr.where(std_ch_sdm_suitability.notnull(), std_ch_sdm_suitability, raster_coarse_interp_std)
-    combined_raster_std = combined_raster_std.rename(species.iloc[0]) # TODO: maybe fix, needed for masking
-    combined_raster_std.rio.set_crs(nsdm_rasters[0].rio.crs, inplace=True)
+        # Reprojecting to desired resolution and masking out land for aquatic species
+        rast = []
+        for combined_raster, name in zip([combined_raster_mean, combined_raster_std], ["mean_suitability", "std_suitability"]):
+            combined_raster = mask_raster(combined_raster, traits, MasksDataset())
 
-    # Reprojecting to desired resolution and masking out land for aquatic species
-    rast = []
-    for combined_raster, name in zip([combined_raster_mean, combined_raster_std], ["mean_suitability", "std_suitability"]):
-        combined_raster = mask_raster(combined_raster, traits, MasksDataset())
-        combined_raster.rio.set_crs(nsdm_rasters[0].rio.crs, inplace=True)
+            # resampling raster
+            lat_resolution, lon_resolution = calculate_resolution(combined_raster)
+            print(f"Original quality resolution: {lon_resolution/1000:0.3f}km")
+            assert lat_resolution == lon_resolution
+            resampling_factor = int(np.ceil(resolution/lat_resolution))
+            combined_raster = coarsen_raster(combined_raster, resampling_factor)
+            combined_raster = combined_raster.rename(name)
+            combined_raster.rio.set_crs(CRS_CH, inplace=True)
 
-        # resampling raster
-        lat_resolution, lon_resolution = calculate_resolution(combined_raster)
-        print(f"Original quality resolution: {lon_resolution/1000:0.3f}km")
-        assert lat_resolution == lon_resolution
-        resampling_factor = int(np.ceil(resolution/lat_resolution))
-        combined_raster = coarsen_raster(combined_raster, resampling_factor)
-        combined_raster = combined_raster.rename(name)
-        rast.append(combined_raster)
-    
-    # Merging to single dataset and saving
-    concatenated = xr.merge(rast).astype("float32")
-    concatenated.rio.set_crs(nsdm_rasters[0].rio.crs, inplace=True)
-    concatenated.attrs["D_m"] = D_m
-    concatenated.attrs["N_species"] = len(nsdm_rasters)
-    concatenated.attrs["species"] = [rast.name for rast in nsdm_rasters]
+            rast.append(combined_raster)
+        
+        # Merging to single dataset and saving
+        concatenated = xr.merge(rast).astype("float32")
+        concatenated.rio.set_crs(CRS_CH, inplace=True)
+        concatenated.attrs["D_m"] = D_m
+        concatenated.attrs["N_species"] = len(nsdm_rasters)
+        concatenated.attrs["species"] = [rast.name for rast in nsdm_rasters]
 
 
-    concatenated.to_netcdf(cache_path)
-    # save_to_netcdf(concatenated, cache_path, scale_factor=1000)
-    # with open(cache_path, "wb") as f:
-    #     pickle.dump((mean_suitability, std_suitability, D_m), f)
-    
-    return concatenated
+        concatenated.to_netcdf(cache_path)
+        # save_to_netcdf(concatenated, cache_path, scale_factor=1000)
+        # with open(cache_path, "wb") as f:
+        #     pickle.dump((mean_suitability, std_suitability, D_m), f)
+        
+        return concatenated
+    else:
+        raise ValueError("No suitability raster found for group")
 
 # TODO: fix epsilon
 def compile_resistance(quality, barriers, eps =  1e-5):